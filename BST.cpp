5bf27641e86b2ff7e9a88e6e3ae7ff7dc38d26d7
ntrappe <33298966+ntrappe@users.noreply.github.com>
2020-11-22 22:18:58 -0800

    Successor + Remove works

--- BST.cpp
+++ BST.cpp
@@ -19,10 +19,13 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 };
 
-
+/**
+ * Height of the BST
+ * Works by finding the longest max subtree
+ **/
 int get_height(TreeNode* root) {
     if(root == nullptr)
-        return -1;
+        return 0;
     else {
         int max_left = get_height(root->left);
         int max_right = get_height(root->right);
@@ -83,10 +86,10 @@ TreeNode* insert(vector<int>& nums) {
 /**
  * Gives you a pointer to the parent node of the value we want
  */
-TreeNode* get_prev(TreeNode* root, int val) {
-    cout << "parent is " << root->val << endl;
+TreeNode* get_parent(TreeNode* root, int val) {
+    //cout << "parent is " << root->val << endl;
     if((root->left && root->left->val == val) || (root->right && root->right->val == val)) {
-        cout << "child has " << val << endl;
+        //cout << "child has " << val << endl;
         return root;
     // either no children left to search or root matches our val 
     // so cannot return anything
@@ -95,21 +98,118 @@ TreeNode* get_prev(TreeNode* root, int val) {
     // keep traversing
     } else {
         if(val < root->val)
-            return get_prev(root->left, val);
-        return get_prev(root->right, val);
+            return get_parent(root->left, val);
+        return get_parent(root->right, val);
     }
     
+}
+
+/**
+ * Finds the smallest value in a subtree (keeps staying left)
+ **/
+TreeNode* min_val(TreeNode* root) {
+    if(!root->left)
+        return root;
+    return min_val(root->left);
+}
+
+/**
+ * Root has 2 kids and we want to delete the root so decide which
+ * kid becomes new root and where the other goes
+ * 
+ * In-order Traversal: right once then all the way left
+ **/
+TreeNode* successor(TreeNode* root) {
+    TreeNode* successor;
+    TreeNode* parent;
+
+    // try to go right first, if can't, return null
+    if(!root->right)
+        successor = nullptr;
+
+    successor = min_val(root->right);
+    parent = get_parent(root, successor->val);
+
+    cout << "successor is " << successor->val << endl;
+    if(parent)
+        cout << "parent is " << parent->val << endl;
+
+    // create a "new successor" by just modifying data
+    parent->val = successor->val;
     
+    // case 1: successor's parent is node to remove so update data (skip over)
+    if(parent == root) {
+        cout << "remove parent node case\n";
+        parent->right = successor->right;
+
+    // case 2: successor's parent NOT to remove so just detach pointer (just remove)
+    } else {
+        cout << "parent (" << parent->val << ") used to point to (" << parent->left->val <<")\n";
+        parent->left = nullptr;
+    }
+   
+    delete successor;
+
+    return root;
 }
 
 /**
  * Worst case O(log n)
  **/
 TreeNode* remove(TreeNode* root, int val) {
-    TreeNode* ref;          // will point to node to remove (for freeing memory)
-    //auto it = root.begin();
+    TreeNode* ref;                      // will point to node to remove (for freeing memory)
+    TreeNode* parent;
+    TreeNode* grandkidL = nullptr;
+    TreeNode* grandkidR = nullptr;
 
-    return nullptr;
+    // case 0: remove root
+    if(root->val == val) {
+        cout << "remove root\n";
+        return successor(root);
+    }
+
+    parent = get_parent(root, val);
+
+    // save node to remove to ref pointer
+    if(val < parent->val)
+        ref = parent->left;
+    ref = parent->right;
+
+    // save kids of node to remove
+    if(ref->left)
+        grandkidL = ref->left;
+    if(ref->right)
+        grandkidR = ref->right;
+
+    // case 1: node to remove has no kids
+    if(!grandkidL && !grandkidR) {
+        parent->left = nullptr;
+        parent->right = nullptr;
+        delete ref;
+    }
+
+    // case 2: node to remove has 1 kid
+    else if(grandkidL && !grandkidR) {
+        parent->left = grandkidL;
+        delete ref;
+    }
+    else if(grandkidR && !grandkidL) {
+        parent->right = grandkidR;
+        delete ref;
+    }
+
+    // case 3: node has 2 kids so choose successor
+    else {
+        // if node to remove is left child, replace it with successor
+        if(parent->left->val == val) {
+            parent->left = successor(ref);
+        // otherwise, replace right child with successor
+        } else {
+            parent->right = successor(ref);
+        }
+    }
+
+    return root;
 }
 
 
@@ -131,7 +231,7 @@ void level_order(TreeNode* root) {
     int level = get_height(root);
     cout << "height of " << level << endl;
 
-    for(int i = 0; i <= level; i++) {
+    for(int i = 1; i <= level; i++) {
         print_level(root, i);
         cout << endl;
     }
@@ -161,13 +261,30 @@ int main() {
     //cout << insert(vect1) << endl;
     //cout << insert(vect2) << endl;
     TreeNode * start = insert(vect3);
-    level_order(start);
-    TreeNode * prev = get_prev(start, 20);
+    //level_order(start);
+    /*
+    TreeNode * prev = get_parent(start, 20);
     if(prev)
         cout << "prev " << prev->val << endl;
     else
-        cout << "didn't find prev\n";
+        cout << "didn't find prev\n";*/
+    //TreeNode * start2 = remove(start, 5);       // try to remove root
     
+    // TEST 4:
+    /**         5
+     *        3    10
+     *      2    8   13
+     *    1    7        15
+     *                    20
+     **/
+    int woop[] = {5, 3, 10, 2, 1, 8, 13, 7, 15, 20};
+    vector<int> vect4(woop, woop + sizeof(woop) / sizeof(woop[0]));
+    start = insert(vect4);
+    TreeNode* start2 = remove(start, 10);
+    level_order(start2);
+
+    //delete(start);
+    //delete(start2);
 
     return EXIT_SUCCESS;
 }
