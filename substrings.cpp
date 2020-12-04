ed28cd8d0896c2c54c9829f794e4db743e3c4524
ntrappe <33298966+ntrappe@users.noreply.github.com>
2020-12-03 20:00:19 -0800

    I hate substrings

---
+++ substrings.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>      /* printf, scanf, puts, NULL */
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+/**
+ * Find the longest repeating, non-overlapping substring
+ */
+
+
+/**
+ * Length of longest substring where the characters in that substring appear an EVEN number of times
+ * ex. 324425 --> 44 or 2442 because characters repeat twice = length of 4
+ * ex. 233015150 --> 33 or or 1515 or 015150 because all charcters repeat twice = length of 6
+ */
+
+/**
+ * BRUTE FORCE: Length of longest substring without repeating characters
+ * Time complexity: O(n^3)
+ */
+int length_LongestSubstring(string str) {
+    int length = 0;                         // length of longest substring
+    return length;
+}
+
+/**
+ * OPTIMIZED: Length of longest substring without repeating characters
+ * Time complexity: O(n)
+ */
+
+
+int main() {
+    return EXIT_SUCCESS;
+}
