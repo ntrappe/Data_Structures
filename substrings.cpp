584ded9e3c3407d64e58779bd9bb37d2ffcfe262
ntrappe <33298966+ntrappe@users.noreply.github.com>
2020-12-03 20:39:44 -0800

    brute force

--- substrings.cpp
+++ substrings.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <set>
 
 using namespace std;
 
@@ -19,11 +20,45 @@ using namespace std;
  */
 
 /**
+ * Helper Method for Length of longest substring
+ * Note: Use sets over hashmap when you just want to submit the value not a key-value pair
+ */
+bool is_unique(string str, int left, int right) {
+    set<char> char_table;                   // keeps track of all chars we've used in this string
+    for(int i = left; i < right; i++) {
+        // if nothing in set, add this char
+        if(char_table.empty()) {
+            char_table.insert(str[i]);
+        // otherwise check if we already have this char, if so then it's repeating so return false
+        } else if(char_table.find(str[i]) != char_table.end()) {
+            return false;
+        } else {
+            char_table.insert(str[i]);
+        }
+    }
+    return true;
+}
+/**
  * BRUTE FORCE: Length of longest substring without repeating characters
  * Time complexity: O(n^3)
  */
 int length_LongestSubstring(string str) {
     int length = 0;                         // length of longest substring
+    for(int i = 0; i < str.length(); i++) {
+        // look at substrings from position i to current pos of j (possible substrings from i)
+        for(int j = i + 1; j <= str.length(); j++) {
+            if(is_unique(str, i, j)) {
+                cout << "unique: " << str.substr(i,j) << endl;
+                if( (str.substr(i,j)).length() > length ) {
+                    length = (str.substr(i,j)).length();
+                    cout << "new length " << length << endl;
+                }
+            } else {
+                cout << "not unique: " << str.substr(i,j) << endl;
+            }
+        }
+    }
+        
     return length;
 }
 
@@ -34,5 +69,6 @@ int length_LongestSubstring(string str) {
 
 
 int main() {
+    length_LongestSubstring("catca");
     return EXIT_SUCCESS;
 }
