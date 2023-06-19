11df8606badbb4a5c37c9edad7aaa5502aa4d014
ntrappe <33298966+ntrappe@users.noreply.github.com>
2023-06-19 15:05:40 -0700

    Print statements to show usage

--- gender-parity-triangle.cpp
+++ gender-parity-triangle.cpp
@@ -8,6 +8,8 @@
 using namespace std;
 
 double DELTA = 0.1;
+double TRI_SIDE = 5.0;
+double TRI_HEIGHT = 4.330127019;
 
 /*------------------------------------------------------
  % Routine Name: getAreaTriangle
@@ -29,27 +31,6 @@ double getAreaTriangle(double base, double height) {
   }
 
   return (height * base) / 2;
-}
-
-/*------------------------------------------------------
- % Routine Name: getAreaTriangle
- % 
- % Description: Calculates area of an EQUILATERAL 
- %   triangle given one side.
- % Result: Calculates area.
- % Return: Return area as double
- % Parameters:
- %
- % NAME               DESCRIPTION
- % -----------------------------------------------------
- % side               One side of equilateral triangle
- -------------------------------------------------------*/
-double getAreaTriangle(double side) {
-  if (side <= 0) {
-    return EXIT_FAILURE;
-  }
-
-  return ((sqrt (3)) / 4) * (side * side * side);
 }
 
 double getAreaTrapezoid(double top, double bottom, double height) {
@@ -108,7 +89,7 @@ double getTrapTop(double triHeight, double traHeight) {
  -------------------------------------------------------*/
 int trapezoidAreaReverse(double bottom, double triHeight, double area,
                          double &height, double &top) {
-  if (triHeight <= 0 || area <= 0 || bottom <= 0 || bottom > area) {
+  if (triHeight <= 0 || area <= 0 || bottom <= 0) {
     return EXIT_FAILURE;
   }
 
@@ -163,15 +144,110 @@ double divideTrapezoid(double bottom, double height, double portion) {
 
 
 int main() {
-  cout << "Test 2 x 3 triangle should be 3: " << getAreaTriangle(3.0, 2.0) << endl;
-  cout << "Test 2 x 3 x 4 trap should be 10: " << getAreaTrapezoid(2.0, 3.0, 4.0) << endl;
-  cout << "Get trap new top w/ height 2.15 and base 5: " << getTrapTop(4.33013, 2.15) << endl;
-  double height = 0;
-  double top = 0;
-  trapezoidAreaReverse(5, 4.33013, 8.1, height, top);
-  printf("Height should be %.2f, top %.2f, for area %.2f\n", height, top, 8.1); 
-  printf("%.2f of trapezoid, width is %.2f\n", 0.0, divideTrapezoid(5, 2.15, 0)); 
-  printf("%.2f of trapezoid, width is %.2f\n", .30, divideTrapezoid(5, 2.15, .30)); 
+  double Eng1 = 0.185567;
+  double Eng2 = 0.309278;
+  double Senior = 0.2680412;
+  double Principal = 0.1958762;
+  double Partner = 0.03092783;
+
+  double womenEng1 = 0.7777778;
+  double womenEng2 = 0.3333333;
+  double womenSenior = 0.1538461;
+  double womenPrincipal = 0.05263157;
+  double womenPartner = 0.0;
+
+  double womenEng1Part = 0;
+  double womenEng2Part = 0;
+  double womenSeniorPart = 0;
+
+  double triangleArea = 0;
+  double triangleHeight = TRI_HEIGHT;
+  double segmentTop = 0;
+  double segmentBase = TRI_SIDE;
+  double segmentHeight = 0;
+  double valid;
+
+  printf("In an org, there are 5 possible roles: Eng1, Eng2, Senior, "
+         "Principal, and Partner\n");
+  printf("Split a triangle into segments that represent how many people "
+         "work in each of those 5 roles\n");
+  printf("\tEng1 %% of Org:\t\t %.2f\n\tEng2 %% of Org:\t\t %.2f\n"
+         "\tSenior %% of Org:\t %.2f\n\tPrincipal %% of Org:\t %.2f\n"
+         "\tPartner %% of Org:\t %.2f\n\n", Eng1, Eng2, Senior, Principal, Partner);
+
+  printf("Given an equilateral triangle with sides of %.2f\n", TRI_SIDE);
+  triangleArea = getAreaTriangle(TRI_SIDE, TRI_HEIGHT);
+  cout << "Triangle area: " << triangleArea << endl;
+
+  if (triangleArea == EXIT_FAILURE) {
+    cerr << "Invalid triangle area calculation" << endl;
+    return EXIT_FAILURE;
+  }
+ 
+  // Eng 1 Calculations
+  printf("--- SEGMENT 1: Eng1 ---\n");
+  valid = trapezoidAreaReverse(segmentBase, triangleHeight, triangleArea * Eng1,
+                               segmentHeight, segmentTop);
+  if (valid == EXIT_FAILURE) {
+    cerr << "Invalid area to trapezoid calculation" << endl;
+    return EXIT_FAILURE;
+  }
+  printf("Eng1:\t\t Area (%.2f), Base (%.2f), Top (%.2f), Height (%.2f)\n", 
+          triangleArea * Eng1, segmentBase, segmentTop, segmentHeight); 
+  womenEng1Part = divideTrapezoid(segmentBase, segmentHeight, womenEng1);
+  segmentBase = segmentTop;
+  triangleHeight -= segmentHeight;
+
+  // Eng 2 Calculations
+  printf("--- SEGMENT 2: Eng2 ---\n");
+  valid = trapezoidAreaReverse(segmentBase, triangleHeight, triangleArea * Eng2,
+                               segmentHeight, segmentTop);
+  if (valid == EXIT_FAILURE) {
+    cerr << "Invalid area to trapezoid calculation" << endl;
+    return EXIT_FAILURE;
+  }
+  printf("Eng2:\t\t Area (%.2f), Base (%.2f), Top (%.2f), Height (%.2f)\n", 
+          triangleArea * Eng2, segmentBase, segmentTop, segmentHeight); 
+  womenEng2Part = divideTrapezoid(segmentBase, segmentHeight, womenEng2);
+  segmentBase = segmentTop;
+  triangleHeight -= segmentHeight;
+
+  // Senior Calculations
+  printf("--- SEGMENT 3: Senior ---\n");
+  valid = trapezoidAreaReverse(segmentBase, triangleHeight, triangleArea * Senior,
+                               segmentHeight, segmentTop);
+  if (valid == EXIT_FAILURE) {
+    cerr << "Invalid area to trapezoid calculation" << endl;
+    return EXIT_FAILURE;
+  }
+  printf("Senior:\t\t Area (%.2f), Base (%.2f), Top (%.2f), Height (%.2f)\n", 
+          triangleArea * Senior, segmentBase, segmentTop, segmentHeight); 
+  womenSeniorPart = divideTrapezoid(segmentBase, segmentHeight, womenSenior);
+  segmentBase = segmentTop;
+  triangleHeight -= segmentHeight;
+
+  // Principal Calculations
+  printf("--- SEGMENT 4: Principal ---\n");
+  valid = trapezoidAreaReverse(segmentBase, triangleHeight, triangleArea * Principal,
+                               segmentHeight, segmentTop);
+  if (valid == EXIT_FAILURE) {
+    cerr << "Invalid area to trapezoid calculation" << endl;
+    return EXIT_FAILURE;
+  }
+  printf("Senior:\t\t Area (%.2f), Base (%.2f), Top (%.2f), Height (%.2f)\n\n", 
+          triangleArea * Principal, segmentBase, segmentTop, segmentHeight); 
+  segmentBase = segmentTop;
+  triangleHeight -= segmentHeight;
+
+
+  printf("Parition each of those 5 segments to show the proportion of "
+         "women vs men\n");
+  printf("\tWomen %% of Eng1:\t %.2f\tWidth: %.2f\n\tWomen %% of Eng2:\t %.2f\t"
+         "Width: %.2f\n\tWomen %% of Senior:\t %.2f\tWidth: %.2f\n\tWomen %% of "
+         "Principal:\t %.2f\n\tWomen %% of Partner:\t %.2f\n\n", 
+         womenEng1, womenEng1Part, womenEng2, womenEng2Part, womenSenior, 
+         womenSeniorPart, womenPrincipal, womenPartner);
+  
 
   return EXIT_SUCCESS;
 }
